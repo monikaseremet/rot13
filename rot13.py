68c8a4af7c778f4b9a5d65ce2009f5a82318c34a
Monika <133088030+monikaseremet@users.noreply.github.com>
2023-05-27 11:58:42 +0200

    Completed rot13.py

--- rot13.py
+++ rot13.py
@@ -7,26 +7,31 @@ def rot13():
     string_encrypted = ""
     
     print("Hello!\nThis program can help you to encrypt or decrypt whatever you want.\nIt's called ROT13 cipher.") 
-    type = input("Please choose what do you want to do: encrypt(en) or decrypt(de): ")
     
-    if type.lower() == "en":
-        string_to_encrypt = input("Please type in a string to ecrypt: ")
-        for char in string_to_encrypt:
-            if char == " ":
-                string_decrypted += " "
-                continue
-            index = ascii_lowercase.index(char)
-            string_decrypted += ascii_lowercase[index+13]
-        print(f"Your decrypted string is: {string_decrypted}")
-    
-    elif type.lower() == "de":
-        string_to_decrypt = input("Please type in a string to decrypt: ")
-        for char in string_to_decrypt:
-            if char == " ":
-                string_encrypted += " "
-                continue
-            index = ascii_lowercase.index(char)
-            string_encrypted += ascii_lowercase[index+13]
-        print(f"Your encrypted string is: {string_encrypted}")
+    while True:
+        input = input("Please choose what do you want to do: encrypt(en), decrypt(de) or quit(q): ")
+
+        if input.lower() == "en":
+            string_to_encrypt = input("Please type in a string to ecrypt: ")
+            for char in string_to_encrypt:
+                if char == " ":
+                    string_decrypted += " "
+                    continue
+                index = ascii_lowercase.index(char)
+                string_decrypted += ascii_lowercase[index+13]
+            print(f"Your decrypted string is: {string_decrypted}")
+
+        elif input.lower() == "de":
+            string_to_decrypt = input("Please type in a string to decrypt: ")
+            for char in string_to_decrypt:
+                if char == " ":
+                    string_encrypted += " "
+                    continue
+                index = ascii_lowercase.index(char)
+                string_encrypted += ascii_lowercase[index+13]
+            print(f"Your encrypted string is: {string_encrypted}")
+            
+        elif input.lower() == "q":
+            break
 
 rot13()
